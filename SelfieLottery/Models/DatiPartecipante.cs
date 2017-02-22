2e4e4722207cbfa025edfea2c3e312a07460f8f1
Luca Congiu <congiuluc>
2017-02-22 14:01:32 +0100

    New App

---
+++ SelfieLottery/Models/DatiPartecipante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SelfieLottery.Models
+{
+    public class DatiPartecipante
+    {
+        public string Id{ get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cognome { get; set; }
+
+        public string ImageUrl { get; set; }
+
+
+    }
+}
